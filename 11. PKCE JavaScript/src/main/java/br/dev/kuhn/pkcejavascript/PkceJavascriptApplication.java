673f244a951c29a123575bbc553a50c0613d8875
Jardel Kuhn <jardelkuhn@gmail.com>
2023-01-29 09:05:22 -0300

    pkce client in javascript public app

---
+++ 11. PKCE JavaScript/src/main/java/br/dev/kuhn/pkcejavascript/PkceJavascriptApplication.java
@@ -0,0 +1,13 @@
+package br.dev.kuhn.pkcejavascript;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkceJavascriptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PkceJavascriptApplication.class, args);
+	}
+
+}
---
+++ 11. PKCE JavaScript/src/test/java/br/dev/kuhn/pkcejavascript/PkceJavascriptApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.kuhn.pkcejavascript;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PkceJavascriptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
